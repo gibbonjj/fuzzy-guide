6a168be8adc3d627ff4dc0c3a4b4a924375b2474
James Gibbons <gibbonjj@gmail.com>
2019-12-01 00:54:05 -0800

    add

--- weatherzzz/ViewController.swift
+++ weatherzzz/ViewController.swift
@@ -23,18 +23,26 @@ struct CellData {
 
 class ViewController: UIViewController, UITableViewDelegate, UISearchControllerDelegate, UITableViewDataSource, UISearchResultsUpdating, UISearchBarDelegate, UIScrollViewDelegate  {
     @IBOutlet weak var pageControl: UIPageControl!
-    
     @IBOutlet weak var mainDataLanding: UIView!
+    @IBOutlet weak var cityList: UITableView!
+    @IBOutlet weak var weeklyDataTable: UITableView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var currentStatus: UILabel!
+    @IBOutlet weak var currentTemp: UILabel!
+    @IBOutlet weak var currentLocation: UILabel!
+    @IBOutlet weak var currentHumidity: UILabel!
+    @IBOutlet weak var currentWindSpeed: UILabel!
+    @IBOutlet weak var currentVisbility: UILabel!
+    @IBOutlet weak var currentPressure: UILabel!
+    
+    @IBOutlet weak var weeklySum: UIImageView!
 
     let searchController = UISearchController(searchResultsController: nil)
-
     var weeklyDataCells = [CellData]()
     private let networkClient = NetworkClient()
     private let locationManager = CLLocationManager()
-    @IBOutlet weak var cityList: UITableView!
-    @IBOutlet weak var weeklyDataTable: UITableView!
     var weeklyData:JSON = [:]
-    
+    var currentlyData:JSON = [:]
     var cities: [String] = Array()
     var city: String = ""
     var state: String = ""
@@ -44,16 +52,28 @@ class ViewController: UIViewController, UITableViewDelegate, UISearchControllerD
     var localData: JSON = [:]
     var time: Double = 0.0
     var frame = CGRect(x:0,y:0,width:0,height:0)
-
-    @IBOutlet weak var scrollView: UIScrollView!
-    
-
     let autoCompleteUrl = "https://weatherservice571.azurewebsites.net/"
+    
+    
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         SwiftSpinner.show("Loading...")
+        //** Location **
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        
+        let lastLocation = locationManager.location
+        debugPrint(lastLocation)
+        
+        
+        
+        
+        
+        
+        
         //******** PageControl
         
         pageControl.numberOfPages = 4
@@ -105,10 +125,7 @@ class ViewController: UIViewController, UITableViewDelegate, UISearchControllerD
         cities.append("Las Vegas")
 
         
-        //** Location **
-        locationManager.delegate = self
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.requestLocation()
+
 
 //        guard let getUrl = URL(string: "https://weatherservice571.azurewebsites.net/street/819%20Santee%20St/city/Los%20Angeles/state/California") else {
 //            return
@@ -175,7 +192,7 @@ class ViewController: UIViewController, UITableViewDelegate, UISearchControllerD
                 return weeklyCount
             }
             else {
-                return 0
+                return 7
             }
         }
         else {
@@ -194,8 +211,67 @@ class ViewController: UIViewController, UITableViewDelegate, UISearchControllerD
         }
         else if tableView == weeklyDataTable {
 
-            let cell = tableView.dequeueReusableCell(withIdentifier: "WeekData")
-      
+            let cell = tableView.dequeueReusableCell(withIdentifier: "WeekData") as! WeeklyData
+            
+            debugPrint(self.weeklyData)
+            
+            if let unixTime = self.weeklyData[indexPath.row]["time"].double {
+                let formatter = DateFormatter()
+                formatter.dateFormat = "MM/dd/yyy"
+                cell.weekDate.text = formatter.string(from: NSDate(timeIntervalSince1970: unixTime) as Date)
+            }
+            
+            if let sunsetTime = self.weeklyData[indexPath.row]["sunsetTime"].double {
+                let formatter = DateFormatter()
+                formatter.dateFormat = "HH:mm"
+                cell.weekSunDown.text = formatter.string(from: NSDate(timeIntervalSince1970: sunsetTime) as Date)
+            }
+            
+            if let sunriseTime = self.weeklyData[indexPath.row]["sunriseTime"].double {
+                let formatter = DateFormatter()
+                formatter.dateFormat = "HH:mm"
+                cell.weekSunUp.text = formatter.string(from: NSDate(timeIntervalSince1970: sunriseTime) as Date)
+            }
+            
+            let icon = self.weeklyData[indexPath.row]["icon"]
+            if(icon == "partly-cloudy-night") {
+                let img = UIImage(named: "weather-night-partly-cloudy")
+                cell.weekSummary.image = img
+            }
+            else if (icon == "clear-day") {
+                let img = UIImage(named: "weather-sunny")
+                cell.weekSummary.image = img
+            }
+            else if (icon == "rain") {
+                let img = UIImage(named: "weather-rainy")
+                cell.weekSummary.image = img
+            }
+            else if (icon == "snow") {
+                let img = UIImage(named: "weather-snowy")
+                cell.weekSummary.image = img
+            }
+            else if (icon == "sleet") {
+                let img = UIImage(named: "weather-snowy-rainy")
+                cell.weekSummary.image = img
+            }
+            else if (icon == "wind") {
+                let img = UIImage(named: "weather-windy")
+                cell.weekSummary.image = img
+            }
+            else if (icon == "fog") {
+                let img = UIImage(named: "weather-fog")
+                cell.weekSummary.image = img
+            }
+            else if (icon == "cloudy") {
+                let img = UIImage(named: "weather-cloudy")
+                cell.weekSummary.image = img
+            }
+            else if (icon == "partly-cloudy-day") {
+                let img = UIImage(named: "weather-partly-cloudy")
+                cell.weekSummary.image = img
+            }
+            
+            
             // cell?.textLabel?.text = cities[indexPath.row]
 //            if let unixTime = self.weeklyData["data"][indexPath.row]["time"].double {
 //                let formatter = DateFormatter()
@@ -217,8 +293,8 @@ class ViewController: UIViewController, UITableViewDelegate, UISearchControllerD
 //                cell.sunUpTime = formatter.string(from: NSDate(timeIntervalSince1970: sunriseTime) as Date)
 //            }
 //            debugPrint(cell.sunDownTime)
-//            cell.layoutIfNeeded()
-            return cell!
+            cell.layoutIfNeeded()
+            return cell as UITableViewCell
         }
         else {
             return UITableViewCell()
@@ -252,6 +328,16 @@ class ViewController: UIViewController, UITableViewDelegate, UISearchControllerD
 //            thirdViewController.city = self.city
 //            thirdViewController.state = self.state
         }
+        else if(segue.identifier == "localClick") {
+
+                        let barViewControllers = segue.destination as! UITabBarController
+                        let destinationViewController = barViewControllers.viewControllers![0] as! SecondViewController
+                        destinationViewController.city = self.city
+                        destinationViewController.state = self.state
+                        let thirdViewController = barViewControllers.viewControllers![2] as! ThirdViewController
+                        thirdViewController.city = self.city
+                        thirdViewController.state = self.state
+        }
     }
 
 }
@@ -274,12 +360,78 @@ extension ViewController : CLLocationManagerDelegate {
         networkClient.fetch(localUrl) { (json, error) in
             if error != nil {
                 debugPrint(error)
+                SwiftSpinner.hide()
             } else {
                 self.localData = json!
                 let jsonUnwrapped = JSON(json)
-                self.weeklyData = jsonUnwrapped["daily"]
+                self.weeklyData = jsonUnwrapped["daily"]["data"]
+                self.currentlyData = jsonUnwrapped["currently"]
+                let icon = self.currentlyData["icon"].string!.lowercased()
+                if(icon == "partly-cloudy-night") {
+                    let img = UIImage(named: "weather-night-partly-cloudy")
+                    self.weeklySum.image = img
+                }
+                else if (icon == "clear-day") {
+                    let img = UIImage(named: "weather-sunny")
+                    self.weeklySum.image = img
+                }
+                else if (icon == "rain") {
+                    let img = UIImage(named: "weather-rainy")
+                    self.weeklySum.image = img
+                }
+                else if (icon == "snow") {
+                    let img = UIImage(named: "weather-snowy")
+                    self.weeklySum.image = img
+                }
+                else if (icon == "sleet") {
+                    let img = UIImage(named: "weather-snowy-rainy")
+                    self.weeklySum.image = img
+                }
+                else if (icon == "wind") {
+                    let img = UIImage(named: "weather-windy")
+                    self.weeklySum.image = img
+                }
+                else if (icon == "fog") {
+                    let img = UIImage(named: "weather-fog")
+                    self.weeklySum.image = img
+                }
+                else if (icon == "cloudy") {
+                    let img = UIImage(named: "weather-cloudy")
+                    self.weeklySum.image = img
+                }
+                else if (icon == "partly-cloudy-day") {
+                    let img = UIImage(named: "weather-partly-cloudy")
+                    self.weeklySum.image = img
+                }
+                
+                if let summary = self.currentlyData["summary"].string {
+                    self.currentStatus.text = summary
+                }
+                
+                if let currentTemp = self.currentlyData["temperature"].double {
+                    self.currentTemp.text = String(Int(currentTemp.rounded())) + " ˚F"
+                }
+                
+                if let currentHumidity = self.currentlyData["humidity"].double {
+                    self.currentHumidity.text = String((currentHumidity * 100.0).rounded()) + " %"
+                }
+                
+                if let currentWind = self.currentlyData["windSpeed"].double {
+                    self.currentWindSpeed.text = String(format: "%.2f", currentWind) + " mph"
+                }
+                
+                if let currentVis = self.currentlyData["visibility"].double {
+                    self.currentVisbility.text = String(format: "%.2f", currentVis) + " km"
+                }
+                
+                if let currentPres = self.currentlyData["pressure"].double {
+                    self.currentPressure.text = String(format: "%.1f", currentPres) + " mb"
+                }
+                
+                debugPrint(self.currentlyData)
                 DispatchQueue.main.async {
                     self.weeklyDataTable.reloadData()
+                    SwiftSpinner.hide()
                 }
             }
         }
